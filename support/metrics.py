3ae079d733759455ab5b9eadf74c5f925593cb1f
Niklas Stoehr <n.stoehr@outlook.com>
2019-07-01 19:16:45 +0100

    added keras dgl and prototype_gnn

--- support/metrics.py
+++ support/metrics.py
@@ -44,13 +44,55 @@ def compute_mig(v_array, z_array):  # ground truth, latent variables
     return mig
 
 
+## metric for measuring the disentanglement - Mutual Information Gap (MIG)
+
+def compute_mig2(v_array, z_array):  # ground truth, latent variables
+
+    ## reshape input data______________________________________
+
+    if len(v_array.shape) <= 1:  # pad the array
+        v_array = np.expand_dims(v_array, axis=0)
+    else:
+        v_array = np.reshape(v_array, (v_array.shape[1], v_array.shape[0]))
+
+    if len(z_array.shape) <= 1:  # pad the array
+        z_array = np.expand_dims(z_array, axis=0)
+    else:
+        z_array = np.reshape(z_array, (z_array.shape[1], z_array.shape[0]))
+
+    ## MIG Computation__________________________________________
+
+    mi_diff = 0
+    dim_diff = 1
+    mi = np.zeros((v_array.shape[0], z_array.shape[0]))
+
+    for i in range(v_array.shape[0]):  # ground truth
+        for j in range(z_array.shape[0]):  # latent variables
+            mi[i, j] = compute_mi(v_array[i], z_array[j])
+
+    for i, v in enumerate(v_array):  # ground truth
+        ## computation of MIG
+        z_order = np.argsort(mi[i,])[::-1]  # descending
+        maximum_mi = mi[i, z_order[0]]
+
+        for j in range(z_array.shape[0]):  # latent variables
+
+            mi_diff += mi[i, j] / maximum_mi
+            dim_diff += np.abs(z_array.shape[0] - v_array.shape[0])
+
+    mig = 1 + ((mi_diff - (maximum_mi * v_array.shape[0])) / dim_diff)
+
+    print("Mutual Information Gap:", mig)
+    return mig
 
 
 ## measure of the mutual dependence between the two variables
 
 def compute_mi(v, z):
-    # mi = np.log(np.sum(v@z)) #+ entropy(z)
+    # mi = np.log(np.sum(v) + np.sum(z)) + entropy(z)
+    # mi = np.log(np.sum(z@v)) + entropy(z)
     mi = mutual_info_score(v, z, contingency=None)
+
     # print("mi:", mi)
 
     return mi
